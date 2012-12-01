3eb142050ba3f0581f99c3a8f26259f65686ad81
Brandon Edmark <brandon@imagemanagement.com>
2012-12-01 09:38:20 -0600

    twitter, logo

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,5 +1,7 @@
 class Newsletter < ActiveRecord::Base
   attr_accessible :title, :document, :summary
+  
+  mount_uploader :document, NewsletterDocumentUploader
 
   def self.page(search, page)
     with_permissions_to(:manage).search(search).order("title").paginate(:per_page => 12, :page => page)
