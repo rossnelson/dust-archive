58fa4cd31b620bb9b5306185ff79958086ad46b7
Ross Nelson <axcess1@me.com>
2013-04-20 00:30:16 -0400

    ckeditor filename to data_file_name

--- vendor/gems/dust/app/models/ckeditor/attachment_file.rb
+++ vendor/gems/dust/app/models/ckeditor/attachment_file.rb
@@ -2,6 +2,6 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
   mount_uploader :data, CkeditorAttachmentFileUploader, :mount_on => :data_file_name
 
   def url_thumb
-    @url_thumb ||= Ckeditor::Utils.filethumb(filename)
+    @url_thumb ||= Ckeditor::Utils.filethumb(data_file_name)
   end
 end
